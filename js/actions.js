71098cffa460663fb2f813ab40e03b1dec0d4a06
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2020-01-09 11:37:48 -0400

    Add files via upload

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,3 @@
-// adiciona mascaras
-$(document).ready(function () {
-    
-
-});
 
 $('.descAdd').blur(function (e) { 
     $('.descAdd').val(parseFloat( $('.descAdd').val()).toFixed(2));
@@ -12,7 +7,28 @@ $('.descAdd').blur(function (e) {
 function atualizar(){
     $(".btAdd").width( $(".btAdd").height());
 };
+// clicks
+$(".btAdd").click(function (e) { 
+    if($(".titleAdd").val().length!=0&&$(".descAdd").val()!=0){
+        add($(".titleAdd").val(),$(".descAdd").val());
+        $(".descAdd").val('');
+        $(".titleAdd").val('');
+    }
+    else{
+        alert("CAMPOS VAZIOS !");
+    }
 
+    
+});
+// fução de add results
+function add(title,quant){
+    $(".result").prepend("<div class='resposta'><div class='titleResp'>"+title+"</div> <div class='quantResp'>"+quant+"</div> </div>");
+    var soma=0;
+    $(".resposta").each(function (i) { 
+        soma+= parseFloat($(".quantResp").eq(i).text());
+    });
+    $(".soma").html("R$ "+soma.toFixed(2));
+}
 // atualizar as telas
 atualizar();
 $(window).scroll(function () { 
